d205b898bae53f59d39c3532bc84ce65e64705be
m98jk <al7joker111@gmail.com>
2020-05-15 15:21:40 +0300

    firist deply 😃

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './style.css';
+import App from './admin/Dashboard';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
